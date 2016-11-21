8dce9af967e2938d119c1360116231e33f1637dd
gforster1993 <gforster1993@gmail.com>
2016-11-21 14:11:03 +0000

    Update angular-dc.js

--- src/angular-dc.js
+++ src/angular-dc.js
@@ -1,16 +1,18 @@
-'use strict';
-
-var angularDc = angular.module('angularDc', []);
-
+ var angularDc = angular.module('angularDc', []);
 /* The main directive in angularDc, responsible creating Dc.js charts.
 
-       The goal of this directive is to provide a AngularJs interface to
-       the existing features of Dc.js.  */
-angularDc.directive('dcChart', ['$timeout',
+   The goal of this directive is to provide a AngularJs interface to
+   the existing features of Dc.js.  */
+angularDc.directive('dcChart', [
+    '$timeout',
     function($timeout) {
-
         /* Whitelisted options to be read from a chart's html attributes. */
-        var directiveOptions = ['name',
+        var directiveOptions = [
+            'dimension',
+            'group',
+            'width',
+            'height',
+            'name',
             'onRenderlet',
             'onFiltered',
             'onPostRedraw',
@@ -20,49 +22,32 @@ angularDc.directive('dcChart', ['$timeout',
             'onZoomed',
             'postSetupChart'
         ];
-
-
         /* Called during the directive's linking phase, this function creates
-       a Dc.js chart. The chart is configured based on settings read from
-       the $scope and the html element.
-     */
-
+   a Dc.js chart. The chart is configured based on settings read from
+   the $scope and the html element.
+ */
         function setupChart(scope, iElement, iAttrs, options) {
-
             // Get the element this directive blongs to, the root of chart
             var chartElement = iElement[0],
-
                 // Get the chart type to create
                 // Rather than creating a directive for each type of chart
                 // we take it as our main parameter, and use that to call the correct Dc.js
                 // chart constructor
                 chartType = iAttrs.dcChart,
-
                 // Get the Dc.js 'Chart Group', if any, for this chart.
                 // Charts within a group are tied together
                 chartGroupName = iAttrs.dcChartGroup || undefined;
-
             // Get the chart creation function for the chartType
             var chartFactory = dc[chartType];
-
             // Create an unconfigured instance of the chart
             var chart = chartFactory(chartElement, chartGroupName);
-
             // Get the potential set of options for this chart
             // Used for mapping chartElement's html attributes to chart options
             var validOptions = getValidOptionsForChart(chart);
-
             // Get additional options from chartElement's html attributes.
             // All options are prepended with 'dc-'' to avoid clashing with html own meaning (e.g width)
             // All options are parsed in angular's $parse language, so beware, it is not javascript!
             options = getOptionsFromAttrs(scope, iAttrs, validOptions);
-
-            // we may have a dc-options attribute which contain a javascript object for stuff
-            // not writtable in $parse language
-            if ('options' in options) {
-                options = _.merge(options, options.options);
-                options.options = undefined;
-            }
             // If we have a dc-name attribute, we populate the scope with the chart
             // object dc-name
             if ('name' in options) {
@@ -71,40 +56,34 @@ angularDc.directive('dcChart', ['$timeout',
             }
             // Configure the chart based on options
             chart.options(options);
-
             // Get event handlers, if any, from options
-            var eventHandlers = _({
+            var eventHandlers = {
                 'renderlet': options.onRenderlet,
                 'preRender': options.onPreRender,
                 'postRender': options.onPostRender,
                 'preRedraw': options.onPreRedraw,
                 'postRedraw': options.onPostRedraw,
-                'filtered': options.onFiltered,
-                'zoomed': options.onZoomed,
-            }).omit(_.isUndefined);
-
+                'filtered.monitor': options.onFiltered,
+                'zoomed': options.onZoomed
+            }
             // Register the eventHandlers with the chart (Dc.js)
-            eventHandlers.each(function(handler, evt) {
-                chart.on(evt, handler);
+            Object.keys(eventHandlers).forEach(function(evt) {
+                if (eventHandlers[evt]) {
+                    chart.on(evt, eventHandlers[evt]);
+                }
             });
-
             // Run the postSetupChart callback, if provided
             if (_.isFunction(options.postSetupChart)) {
                 options.postSetupChart(chart, options);
             }
-
             return chart;
         }
 
         function getValidOptionsForChart(chart) {
-
             // all chart options are exposed via a function
-            return _(chart).functions()
-                .extend(directiveOptions)
-                .map(function(s) {
-                    return 'dc' + s.charAt(0).toUpperCase() + s.substring(1);
-                })
-                .value();
+            return _(chart).functions().extend(directiveOptions).map(function(s) {
+                return 'dc' + s.charAt(0).toUpperCase() + s.substring(1);
+            }).value();
         }
 
         function getOptionsFromAttrs(scope, iAttrs, validOptions) {
@@ -117,9 +96,17 @@ angularDc.directive('dcChart', ['$timeout',
                     if (key.substring(0, 2) === 'dc') {
                         key = key.charAt(2).toLowerCase() + key.substring(3);
                     }
-                    return [key, value];
+                    return [
+                        key,
+                        value
+                    ];
                 })
-                .zipObject()
+                .fromPairs()
+                .tap(function(options) {
+                    _.merge(options, options.options);
+                    delete options.options;
+                })
+                .omitBy(_.isNil)
                 .value();
         }
         return {
@@ -138,29 +125,27 @@ angularDc.directive('dcChart', ['$timeout',
                 };
                 // watch for the scope to settle until all the attributes are defined
                 var unwatch = scope.$watch(function() {
-                    var options = _(iAttrs.$attr)
-                        .keys()
-                        .filter(function(s) {
-                            return s.substring(0, 2) === 'dc' && s !== 'dcChart' && s !== 'dcChartGroup';
-                        })
-                        .map(function(key) {
-                            try {
-                                // We ignore exception waiting for the data to be potentially loaded
-                                // by the controller
-                                var r = scope.$eval(iAttrs[key]);
-                                if (_.isUndefined(r)) {
-                                    throw new Error(iAttrs[key] + ' is undefined');
-                                }
-                                return r;
-                            } catch (e) {
-                                if (printExceptions) {
-                                    console.log('unable to eval' + key + ':' + iAttrs[key]);
-                                    throw e;
-                                }
-                                return undefined;
+                    var options = _(iAttrs.$attr).keys().filter(function(s) {
+                        return s.substring(0, 2) === 'dc' && s !== 'dcChart' && s !== 'dcChartGroup';
+                    }).map(function(key) {
+                        try {
+                            // We ignore exception waiting for the data to be potentially loaded
+                            // by the controller
+                            var r = scope.$eval(iAttrs[key]);
+                            if (_.isUndefined(r)) {
+                                return false;
+                                // throw Error(iAttrs[key] + ' is undefined');
                             }
-                        });
-                    if (options.any(_.isUndefined)) {
+                            return r;
+                        } catch (e) {
+                            if (printExceptions) {
+                                console.log('unable to eval' + key + ':' + iAttrs[key]);
+                                throw e;
+                            }
+                            return undefined;
+                        }
+                    });
+                    if (options.some(_.isUndefined)) {
                         // return undefined if there is at least one undefined option
                         // so that the $watch dont call us again at this $digest time
                         return undefined;
@@ -170,16 +155,16 @@ angularDc.directive('dcChart', ['$timeout',
                     if (!_.isUndefined(options)) {
                         // Stop the $watch, as we now created the charts
                         unwatch();
-
                         var chart = setupChart(scope, iElement, iAttrs);
                         // populate the .reset childrens with necessary reset callbacks
-                        var a = angular.element(iElement[0].querySelector('a.reset'));
+                        var a = angular.element(iElement[0].querySelector('small.reset'));
                         a.on('click', function() {
                             chart.filterAll();
                             dc.redrawAll();
                         });
                         a.attr('href', 'javascript:;');
                         a.css('display', 'none');
+                        a.css('cursor', 'pointer');
                         // watching the attributes is costly, so we stop after first rendering
                         chart.render();
                     }
@@ -189,24 +174,20 @@ angularDc.directive('dcChart', ['$timeout',
                 $timeout(function() {
                     printExceptions = true;
                 }, 2000);
-
             }
         };
-
     }
 ]);
-
 /* Another directive, responsible of integration angular's select directive with dc.
 
-    <dc-select dc-dimension="fooDimension", all-label="All foos"/>
+<dc-select dc-dimension="fooDimension", all-label="All foos"/>
 
-   This directive helps to filter by arbitrary dimensions without need for another graph.
+This directive helps to filter by arbitrary dimensions without need for another graph.
 
-   Note that if there is a graph on the same dimension as the select, changing the select value
-   will not update the graph's own selection. This is also the case if you make 2 graphs
-   with same dimension. This is a limitation of the underlying lib dc.js
+Note that if there is a graph on the same dimension as the select, changing the select value
+will not update the graph's own selection. This is also the case if you make 2 graphs
+with same dimension. This is a limitation of the underlying lib dc.js
 */
-
 angularDc.directive('dcSelect', [
     function() {
         return {
@@ -224,8 +205,8 @@ angularDc.directive('dcSelect', [
                         chart = dc.baseMixin({});
                         chart.dimension(dimension);
                         chart.group(dimension);
-                        chart._doRender = function() {};
-                        chart._doRedraw = function() {};
+                        chart._doRender = function() { };
+                        chart._doRedraw = function() { };
                         scope.selectModel = {
                             key: scope.allLabel
                         };
